bef49e112404c169cec86766a3a9c1056d863b92
gihoonmx-source <gihoon.mx@gmail.com>
2026-07-01 18:39:05 +0900

    update: API key changed

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
    =================================================== */
 
 const CONFIG = {
-  GOOGLE_MAPS_API_KEY: 'AIzaSyBqslqTAzLGmFZvl_IJha003aR0OXizZoI',
+  GOOGLE_MAPS_API_KEY: 'AIzaSyAA6AUH2Ks59_dG2Rpu5_Rb1EfCg20E7bk',
   MAP_ID: '7a95a61439a56b08dfcf4557',
   MAP_CENTER_LAT: 37.38,
   MAP_CENTER_LNG: 127.05,
